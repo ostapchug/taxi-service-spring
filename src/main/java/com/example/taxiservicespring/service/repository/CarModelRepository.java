bde79e8667dcb74ac336e1967292965630402e46
Taras Ostapchuk <ostapchug@gmail.com>
2022-07-18 21:17:21 +0300

    change car model repository to use jdbc template

--- src/main/java/com/example/taxiservicespring/service/repository/CarModelRepository.java
+++ src/main/java/com/example/taxiservicespring/service/repository/CarModelRepository.java
@@ -1,8 +1,10 @@
 package com.example.taxiservicespring.service.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 import com.example.taxiservicespring.service.model.CarModel;
 
-public interface CarModelRepository extends JpaRepository<CarModel, Long>{
+public interface CarModelRepository {
+    
+    Optional<CarModel> findById(long id);
 }
---
+++ src/main/java/com/example/taxiservicespring/service/repository/impl/JdbcCarModelRepository.java
@@ -0,0 +1,37 @@
+package com.example.taxiservicespring.service.repository.impl;
+
+import java.util.Optional;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.taxiservicespring.service.model.CarModel;
+import com.example.taxiservicespring.service.repository.CarModelRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcCarModelRepository implements CarModelRepository{
+    
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Optional<CarModel> findById(long id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM car_model WHERE id = ?", 
+                (rs, rowNum) ->  Optional.of(
+                        new CarModel(
+                                rs.getLong("id"),
+                                rs.getString("brand"),
+                                rs.getString("name"),
+                                rs.getString("color"),
+                                rs.getInt("year"),
+                                rs.getInt("seat_count")
+                                )
+                        ),
+                id
+                );
+    }
+
+}
